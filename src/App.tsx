80226b4dea32c64d0996f6e7a4e16ba8f4d9a6ce
SARB <mrsinghbusiness05@gmail.com>
2023-09-27 07:16:22 +0530

    folder strcutured + routing completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { BrowserRouter as  Router , Route , Routes  } from "react-router-dom" ;
+import Header from "./Components/Header";
 
+  import { lazy , Suspense } from "react";
+
+  const Home     = lazy(() => import('./Components/Home'));
+  const Learning = lazy(() => import('./Components/Learning'));
+  const Quiz     = lazy(() => import('./Components/Quiz'));
+  const Result   = lazy(() => import('./Components/Result'));
+  const Login    = lazy(() => import('./Components/Login'));
+  const Loader   = lazy(() => import('./Components/Loader'));
 
 function App() {
 
   return (
-    <>
-      First Step 
-    </>
+        <Router>
+          <Header />
+            <Suspense fallback = {<Loader />}>
+            <Routes> 
+               <Route path = "/"  element = {<Home />} />
+               <Route path = "/learn"  element = {<Learning />} />
+               <Route path = "/quiz"  element = {<Quiz />} />
+               <Route path = "/result"  element = {<Result />} />
+               <Route path = "/login"  element = {<Login  />} />
+            </Routes>
+            </Suspense>
+        </Router>
   )
 }
 
